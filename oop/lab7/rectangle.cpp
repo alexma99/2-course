374d70204b04378a63b0446da9c320ee09989638
alexma99 <31708285+alexma99@users.noreply.github.com>
2017-12-22 01:02:51 +0300

    Create rectangle.cpp

---
+++ oop/lab7/rectangle.cpp
@@ -0,0 +1,89 @@
+#include "rectangle.h"
+#include <cmath>
+#include <iostream>
+
+Rectangle::Rectangle() {
+	side_a = 0.0;
+	side_b = 0.0;
+}
+
+Rectangle::Rectangle(size_t i, size_t j) : side_a(i), side_b(j) {
+	//	std::cout << "Создан прямоугольник со сторонами: " << side_a << ", " << side_b << std::endl;
+}
+
+Rectangle::Rectangle(std::istream& is) {
+	std::cout << "Введите значение a:";
+	while (!(is >> side_a)) {
+		std::cout << "Неверный ввод" << std::endl;
+		is.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "Введите значение a:";
+	}
+	std::cout << "Введите значение b:";
+	while (!(is >> side_b)) {
+		std::cout << "Неверный ввод" << std::endl;
+		is.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "Введите значение b:";
+	}
+}
+
+Rectangle::Rectangle(const Rectangle &orig) {
+	side_a = orig.side_a;
+	side_b = orig.side_b;
+}
+
+Rectangle& Rectangle::operator=(const Rectangle& right) {
+	if (this == &right)
+		return *this;
+	side_a = right.side_a;
+	side_b = right.side_b;
+	return *this;
+}
+
+bool Rectangle::operator == (const Rectangle& other)
+{
+	return side_a == other.side_a && side_b == other.side_b;
+}
+
+Rectangle& Rectangle::operator++() {
+	side_a++;
+	side_b++;
+	return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const Rectangle& obj) {
+	os << "Прямоугольник со сторонами ";
+	os << "[" << obj.side_a <<"," << obj.side_b << "], Площадь = " << obj.side_a*obj.side_b << std::endl;
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, Rectangle& obj) {
+	std::cout << "Введите значение a:";
+	while (!(is >> obj.side_a)) {
+		std::cout << "Неверный ввод" << std::endl;
+		is.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "Введите значение a:";
+	}
+	std::cout << "Введите значение b:";
+	while (!(is >> obj.side_b)) {
+		std::cout << "Неверный ввод" << std::endl;
+		is.clear();
+		while (std::cin.get() != '\n');
+		std::cout << "Введите значение b:";
+	}
+	return is;
+}
+
+double Rectangle::Square() {
+	return side_a*side_b;
+}
+
+void Rectangle::print(std::ostream & os) const {
+	os << *this;
+}
+
+//Rectangle::~Rectangle() {
+//
+//}
