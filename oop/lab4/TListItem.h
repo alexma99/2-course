382d36925b02d47e5a3013797f57452612fb0b9e
alexma99 <31708285+alexma99@users.noreply.github.com>
2017-12-21 13:07:03 +0300

    Create TListItem.h

---
+++ oop/lab4/TListItem.h
@@ -0,0 +1,27 @@
+#ifndef TLISTITEM_H
+#define TLISTITEM_H
+
+#include <memory>
+#include "FSquare.h"
+#include "Triangle.h" 
+#include "Rectangle.h" 
+
+template <class T>
+class TListItem {
+public:
+	TListItem(const std::shared_ptr<T> &obj);
+	std::shared_ptr<TListItem<T>> GetNext();
+	std::shared_ptr<TListItem<T>> GetPrev();
+	void SetNext(std::shared_ptr<TListItem<T>> item);
+	void SetPrev(std::shared_ptr<TListItem<T>> prev);
+	template <class A> friend std::ostream& operator<<(std::ostream &os, const TListItem<A> &obj);
+	std::shared_ptr<T> GetFigure() const;
+	virtual ~TListItem() {};
+private:
+	std::shared_ptr<T> item;
+	std::shared_ptr<TListItem<T>> next;
+	std::shared_ptr<TListItem<T>> prev;
+};
+
+#endif
+
