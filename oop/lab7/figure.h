810fab46fc57ae0af0ad9cf78890def581192af3
alexma99 <31708285+alexma99@users.noreply.github.com>
2017-12-22 00:58:20 +0300

    Create figure.h

---
+++ oop/lab7/figure.h
@@ -0,0 +1,16 @@
+#ifndef FIGURE_H
+#define FIGURE_H
+#include <iostream>
+#include <memory>
+
+class Figure {
+public:
+	virtual void print(std::ostream& os) const = 0;
+	virtual double Square() = 0;
+	bool TypedEquals(std::shared_ptr<Figure> other);
+	bool SquareLess(std::shared_ptr<Figure> other);
+	bool SquareGreater(std::shared_ptr<Figure> other);
+	virtual ~Figure() {};
+};
+
+#endif  /*  FIGURE_H  */
